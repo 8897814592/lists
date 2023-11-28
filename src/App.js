6d7f3a0cdbbc3abf91f73488c1b7f8deea355e28
saipriya <saipriyasaipriya342@gmail.com>
2023-11-28 16:11:22 +0000

    doubt

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import UserProfile from './components/UserProfile/index'
+import './App.css'
+
+const userDetailsList = [
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'saipriya',
+    role: 'Software Developer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'saipriya',
+    role: 'Software Developer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'saipriya',
+    role: 'Software Developer',
+  },
+  {
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'saipriya',
+    role: 'Software Developer',
+  },
+]
+
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => {
+        ;<UserProfile userDetails={eachItem} />
+      })}
+    </ul>
+  </div>
+)
+
+export default App
